8fa71208f7f848f9b37f826a8d52f01266fd8f8f
elamahpla <theamazingness@gmail.com>
2018-07-13 19:08:41 -0500

    Using this

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const current = document.querySelector('#current');
-const imgs = document.querySelectorAll('.imgs img');
-const opacity = 0.4;
-
-imgs[0].style.opacity = opacity;
-imgs.forEach(img => img.addEventListener('click', imgClick));
-
-function imgClick(e) {
+const imgClick = (e) => {
   imgs.forEach(img => (img.style.opacity = 1));
   current.src = e.target.src;
-  current.classList.add('fade-in');
+  // current.classList.add('fade-in');
   setTimeout(() => current.classList.remove('fade-in'), 500);
   e.target.style.opacity = opacity;
 }
+
+const current = document.querySelector('#current');
+const imgs = document.querySelectorAll('.imgs img');
+const opacity = 0.6;
+
+imgs[0].style.opacity = opacity;
+imgs.forEach(img => img.addEventListener('click', imgClick));
