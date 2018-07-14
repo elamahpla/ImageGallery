36e229211c7a1e77192cc762a388e996acfec455
elamahpla <theamazingness@gmail.com>
2018-07-14 12:48:10 -0500

    Stuff.

--- todo.js
+++ todo.js
@@ -3,3 +3,6 @@
 // set alt text
 // add lazy loading. 
 // i can use this version until i stop being lazy and use js to map instead of # amount of img tags.
+
+
+// maybe i should just make this in react. /shrug. for now i'll use es6.
