e5a836a6b75d100f3ef2a0fe74de4307944824a8
elamahpla <theamazingness@gmail.com>
2018-07-13 20:21:42 -0500

    Adding webpack and babel so people on older browsers can use if they want.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-const imgClick = (e) => {
-  imgs.forEach(img => (img.style.opacity = 1));
+const imageClick = (e) => {
+  imgs.forEach((img) => (img.style.opacity = 1));
   current.src = e.target.src;
-  // current.classList.add('fade-in');
   setTimeout(() => current.classList.remove('fade-in'), 500);
   e.target.style.opacity = opacity;
 }
@@ -11,4 +10,4 @@ const imgs = document.querySelectorAll('.imgs img');
 const opacity = 0.6;
 
 imgs[0].style.opacity = opacity;
-imgs.forEach(img => img.addEventListener('click', imgClick));
+imgs.forEach((img) => img.addEventListener('click', imageClick));
